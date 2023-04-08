384622ebd35ade48dd37f5658c9b940daac29906
SinHT0108 <somu950108@gmail.com>
2023-04-08 22:29:02 +0900

    del

--- programmers/타겟넘버.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers, target):
-    
-    return dfs(target, 0, numbers)
-
-
-def dfs(target, current, numbers):
-    if not numbers:
-        return 1 if target == current else 0
-    return dfs(target, current + numbers[0], numbers[1:]) + dfs(target, current - numbers[0], numbers[1:])
